e4ba632822f6fbf50ccc6837e4b1ce76a2c78914
hobroker <igor.leahu24@gmail.com>
2020-03-07 21:32:27 +0200

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,8 +34,7 @@ __copy_env() {
 }
 
 prepare() {
-    cat ~/.ssh/id_rsa
-    echo -e "Host ${SSH_HOST}\n\tStrictHostKeyChecking no\n\tIdentityFile ~/.ssh/id_rsa\n" >> ~/.ssh/config
+    __remote_run_silent ls
     docker-compose down
     __copy_env
 
