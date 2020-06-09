b65f168fc012325d2ba7861b8cdd06b03b7b015d
hobroker <igor.leahu24@gmail.com>
2020-06-09 12:43:18 +0300

    Update github actions deploy workflow

--- src/features/spotify/accessors.js
+++ src/features/spotify/accessors.js
@@ -1,7 +1,7 @@
-import { pipe, prop } from 'ramda';
+import { compose, prop } from 'ramda';
 import { SPOTIFY } from './constants';
 import { getConfigFeatures } from '../../accessors/config';
 
 export const getSpotify = prop(SPOTIFY);
 
-export const getSpotifyConfig = pipe(getConfigFeatures, getSpotify);
+export const getSpotifyConfig = compose(getSpotify, getConfigFeatures);
