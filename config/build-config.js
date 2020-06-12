0f6322638f935eb7cee8164e536408d9f7fd1420
hobroker <igor.leahu24@gmail.com>
2020-06-12 13:33:39 +0300

    Update .circleci/config.yml

--- config/build-config.js
+++ config/build-config.js
@@ -18,19 +18,22 @@ if (parameters === undefined) {
   throw new Error(`no parameters for branch ${branch}`);
 }
 
+const readValue = varName => {
+  const sources = [parameters, process.env];
+  for (let i = 0; i < sources.length; i += 1) {
+    const value = sources[i][varName];
+    if (value !== undefined) {
+      return value;
+    }
+  }
+
+  throw new Error(`did not find value for "${varName}"`);
+};
+
 const finalEnv = template.replace(/{{.*}}/g, item => {
   const varName = item.replace(/({{|}})/g, '');
 
-  const value =
-    parameters[varName] !== undefined
-      ? parameters[varName]
-      : process.env[varName];
-
-  if (value === undefined) {
-    throw new Error(`did not find value for "${varName}"`);
-  }
-
-  return value;
+  return readValue(varName);
 });
 
 fs.writeFileSync(finalEnvPath, finalEnv);
