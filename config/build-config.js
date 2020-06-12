e736ecf8236d286714bae9a2098c0c1d27a0276b
hobroker <igor.leahu24@gmail.com>
2020-06-12 13:27:07 +0300

    Update .circleci/config.yml

--- config/build-config.js
+++ config/build-config.js
@@ -2,12 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 const branch = process.env.CIRCLE_BRANCH;
+const finalEnvFilename = '.env';
+const templateFilename = '.template.env';
+const parametersFilename = 'parameters.json';
 
-const finalEnvPath = path.join(__dirname, '../.env');
-const templatePath = path.join(__dirname, '.template.env');
-const parametersPath = path.join(__dirname, 'parameters.json');
-const template = fs.readFileSync(templatePath, 'utf8');
-const parameters = JSON.parse(fs.readFileSync(parametersPath, 'utf8'))[branch];
+const read = filename => fs.readFileSync(filename, 'utf8');
+
+const finalEnvPath = path.join(__dirname, '..', finalEnvFilename);
+const templatePath = path.join(__dirname, templateFilename);
+const parametersPath = path.join(__dirname, parametersFilename);
+const template = read(templatePath);
+const parameters = JSON.parse(read(parametersPath))[branch];
 
 if (parameters === undefined) {
   throw new Error(`no parameters for branch ${branch}`);
@@ -16,7 +21,10 @@ if (parameters === undefined) {
 const finalEnv = template.replace(/{{.*}}/g, item => {
   const varName = item.replace(/({{|}})/g, '');
 
-  const value = parameters[varName] || process.env[varName];
+  const value =
+    parameters[varName] !== undefined
+      ? parameters[varName]
+      : process.env[varName];
 
   if (value === undefined) {
     throw new Error(`did not find value for "${varName}"`);
