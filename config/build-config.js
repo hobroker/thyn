31b2f285fbe714f3cc40a9c09f5a293ab999df45
hobroker <igor.leahu24@gmail.com>
2020-06-12 13:02:23 +0300

    Update .circleci/config.yml

---
+++ config/build-config.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const branch = process.env.CIRCLE_BRANCH;
+
+const finalEnvPath = path.join(__dirname, '../.env2');
+const templatePath = path.join(__dirname, '.template.env');
+const parametersPath = path.join(__dirname, 'parameters.json');
+const template = fs.readFileSync(templatePath, 'utf8');
+const parameters = JSON.parse(fs.readFileSync(parametersPath, 'utf8'))[branch];
+
+if (parameters === undefined) {
+  throw new Error(`no parameters for branch ${branch}`);
+}
+
+const finalEnv = template.replace(/{{.*}}/g, item => {
+  const varName = item.replace(/({{|}})/g, '');
+
+  const value = parameters[varName] || process.env[varName];
+
+  if (value === undefined) {
+    throw new Error(`did not find value for "${varName}"`);
+  }
+
+  return value;
+});
+
+fs.writeFileSync(finalEnvPath, finalEnv);
